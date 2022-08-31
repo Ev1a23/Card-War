4386fe6a5a11d6788c394ffd6f97fd9cea934c7b
Eviatar Shemesh <eviatarshemesh23@gmail.com>
2022-08-31 11:30:27 +0300

    Final

---
+++ Types.py
@@ -0,0 +1,9 @@
+# a dictonary to store every card score
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8,
+          'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
+
+# A tuple to store which kinds of suits a card can have
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+
+# A tuple to store which kinds of rank a card can have
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
